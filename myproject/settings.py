f2d7ffe89ef0f2ee75377d9f8612e76f95321e4b
ifycodeng01 <ifeanyi.ugwumba@queryfier.com>
2023-11-02 12:25:18 +0100

    Heroku config

--- myproject/settings.py
+++ myproject/settings.py
@@ -117,5 +117,5 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # AUTH_USER_MODEL = "accounts.CustomUser"
